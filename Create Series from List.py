cf918bf7278215bd96cb02d06f247285a86cae97
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-12 23:59:30 +0600

    Update Create Series from List.py

--- Create Series from List.py
+++ Create Series from List.py
@@ -6,7 +6,6 @@ data_list = ["apple", "banana", "cherry", "date", "elderberry"]
 
 # Create a Pandas Series from data_list
 # YOUR CODE HERE
-
 my_series=pd.Series(data_list)
 
 # Print the created Series
